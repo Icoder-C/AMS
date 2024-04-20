da08e87580b8f71258108f887bf85940b0319ede
Icoder-C <puyalroshan77@gmail.com>
2024-04-20 21:50:16 +0545

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,8 +2,9 @@
 define("BASE_PATH",dirname(__DIR__)); 
 require BASE_PATH."/Utils/functions.php";
 
-require basePath("/Config/database.mysqli.php");
+// require basePath("/Config/database.mysqli.php");
 
+require basePath("/Core/router.php");
 
 // $url = parse_url($_SERVER["REQUEST_URI"])["path"];
 
